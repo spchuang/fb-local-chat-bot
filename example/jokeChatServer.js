0ac6ec7b234e26a99567b015df7ffb20c3dd2935
spchuang <sampingchuang@gmail.com>
2017-03-04 07:52:30 -0800

    Add hscroll button to example

--- example/jokeChatServer.js
+++ example/jokeChatServer.js
@@ -11,18 +11,20 @@ const PostBackTypes = {
   LIST_JOKES: 'LIST_JOKES',
   TELL_RIDDLE: 'TELL_RIDDLE',
   MORE_BUTTONS: 'MORE_BUTTONS',
+  SHOW_HSCROLL: 'SHOW_HSCROLL',
 };
 const QuickReplyTypes = {
   TELL_JOKE: 'TELL_JOKE',
   WRONG_ANSWER: 'WRONG_ANSWER',
   CORRECT_ANSWER: 'CORRECT_ANSWER',
 }
+const catImage = 'http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg';
 // Create 10 Quick Replies, Joke 1, Joke 2, ...
 const JokeQuickReplies = [...Array(10).keys()].map(x => Bot.createQuickReply(`Joke ${x+1}`, 'TELL_JOKE'));
 
 function makeServer() {
   // initialize Bot and define event handlers
-  Bot.init('<TOKEN>', '<VERIFY_TOKEN>', true /*useLocalChat*/);
+  Bot.init('<TOKEN>', '<VERIFY_TOKEN>', true /*useLocalChat*/, false /*useMessenger*/);
 
   Bot.on('text', (event) => {
     const senderID = event.sender.id;
@@ -35,6 +37,7 @@ function makeServer() {
         Bot.createPostbackButton('Solve a riddle', PostBackTypes.TELL_RIDDLE),
         Bot.createURLButton('Open website for jokes', 'http://jokes.cc.com/', 'tall'),
         Bot.createPostbackButton('More buttons', PostBackTypes.MORE_BUTTONS),
+        Bot.createPostbackButton('Show hscroll', PostBackTypes.SHOW_HSCROLL),
       ]
     );
   });
@@ -81,6 +84,38 @@ function makeServer() {
             Bot.createShareButton(),
           ]
         );
+        break;
+      case PostBackTypes.SHOW_HSCROLL:
+        const elements = [
+          Bot.createGenericTemplateElement(
+            'This is a cool hscroll 1',
+            null,
+            null,
+            catImage,
+            null,
+            [
+              Bot.createPostbackButton('button1', ''),
+              Bot.createPostbackButton('button2', ''),
+            ]
+          ),
+          Bot.createGenericTemplateElement(
+            'This is a cool hscroll 2',
+            null,
+            null,
+            catImage,
+            null,
+            [
+              Bot.createPostbackButton('button1', ''),
+              Bot.createPostbackButton('button2', ''),
+            ]
+          ),
+        ];
+
+        Bot.sendGenericTemplate(
+          senderID,
+          elements
+        );
+        break;
     }
   });
 
